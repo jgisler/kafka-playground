e0d5af9b6461a92ff479a20265108743f8a59a56
jim <jim.gisler@gmail.com>
2015-08-14 17:36:35 -0700

    initial project

---
+++ kafka-consumer/src/main/java/org/gislers/kafka/playground/consumer/DefaultConsumer.java
@@ -0,0 +1,14 @@
+package org.gislers.kafka.playground.consumer;
+
+/**
+ * Created by jim on 8/14/15.
+ */
+public class DefaultConsumer implements Runnable {
+
+//    private KafkaStream kafkaStream;
+
+    @Override
+    public void run() {
+
+    }
+}
